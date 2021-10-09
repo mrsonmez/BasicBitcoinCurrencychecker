6fa41fede86a94fdb805671ba0e29e6ed300235b
mrsonmez <92211029+mrsonmez@users.noreply.github.com>
2021-10-09 22:41:26 +0300

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,5 @@ import requests,time
 while True:
     response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
     data = response.json()
-    a = data["bpi"]["USD"]["rate"]
-    print(a)
+    print(data["bpi"]["USD"]["rate"])
     time.sleep(30)
